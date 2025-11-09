2c3c94a281dd43b29b44af41def85d67b24ed357
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-09 23:26:48 +0100

    totalvocales

--- src/E070ContarVocales.java
+++ src/E070ContarVocales.java
@@ -3,6 +3,7 @@ void main () {
 }
 
 void contarVocales () {
+    int vocalesTotales = 0;
     String palabra = IO.readln("\nIntrodcuce una palabra y el programa contará las vocales que tiene: ").replaceAll("\\s" , "").toLowerCase();
 
     if (palabra.isEmpty()) {
@@ -14,14 +15,20 @@ void contarVocales () {
         char caracter = palabra.charAt(i);
         if (caracter == 'a' | caracter == 'á') {
             IO.println(String.format("\nLa palabra %S tiene la vocal a en la posición %d",palabra, i + 1 ));
+            vocalesTotales++;
         } else if (caracter == 'e' | caracter == 'é') {
             IO.println(String.format("\nLa palabra %S tiene la vocal e en la posición %d",palabra, i + 1 ));
+            vocalesTotales++;
         } else if (caracter == 'i' | caracter == 'í') {
             IO.println(String.format("\nLa palabra %S tiene la vocal i en la posición %d",palabra, i + 1 ));
+            vocalesTotales++;
         } else if (caracter == 'o' | caracter == 'ó') {
             IO.println(String.format("\nLa palabra %S tiene la vocal o en la posición %d",palabra, i + 1 ));
+            vocalesTotales++;
         } else if (caracter == 'u' | caracter == 'ú' | caracter == 'ü') {
             IO.println(String.format("\nLa palabra %S tiene la vocal u en la posición %d",palabra, i + 1 ));
+            vocalesTotales++;
         }
     }
+    IO.println(String.format("\nLa palabra %S tiene un total de %d vocales", palabra, vocalesTotales));
 }
