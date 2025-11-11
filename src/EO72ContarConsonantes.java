37cc9fd7069dceea77e7e14eaec8d9f29c5bb475
Carlos Santos <santosjimenezcarlos4@gmail.com>
2025-11-11 22:19:43 +0100

    no contabilizar espacios

--- src/EO72ContarConsonantes.java
+++ src/EO72ContarConsonantes.java
@@ -1,16 +1,27 @@
 void main() {
+    String texto = IO.readln("\nIntroduce una palabra o frase: ")
+                    .toLowerCase()
+                    .trim();
 
-    String texto = IO.readln("\nIntroduce una palabra: ").toLowerCase().trim();
-
-    IO.println(String.format("\nLa palabra/frase " + "%s" + " tiene %d consonantes", texto, contarConsonantes(texto)));
+    IO.println(String.format(
+        "\nLa palabra/frase \"%s\" tiene %d consonantes.",
+        texto,
+        contarConsonantes(texto)
+    ));
 }
 
 int contarConsonantes(String texto) {
-    int totalConsonantes = 0;
+    int totalConsonantes = 0, posicion = 0;
 
     for (int i = 0; i < texto.length(); i++) {
         char c = texto.charAt(i);
-        if (c >= 'b' & c <= 'z' & c != 'e' & c != 'i' & c != 'o' & c != 'u' & c != 'é' & c != 'í' & c != 'ó' & c != 'ú' & c != 'ü' | c == 'ñ') {
+
+        if (c != ' ') {
+            posicion++;
+        }
+
+        if ((c >= 'b' & c <= 'z' & c != 'e' & c != 'i' & c != 'o' & c != 'u') | c == 'ñ') {
+            IO.println(String.format("\nConsonante '%s' en la posición %d", c, posicion));
             totalConsonantes++;
         }
     }
