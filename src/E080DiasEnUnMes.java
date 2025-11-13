42430a3237ed47819b1f489f44ab92e668a24aed
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-13 16:14:18 +0100

    nose

--- src/E080DiasEnUnMes.java
+++ src/E080DiasEnUnMes.java
@@ -1,26 +1,13 @@
 String diasEnUnMes(int mes) {
-
     return switch (mes) {
-        case 0 -> "";
-        case 1 -> "31";
+        case 1, 3, 5, 7, 8, 10, 12 -> "31";
+        case 4, 6, 9, 11 -> "30";
         case 2 -> "28";
-        case 3 -> "31";
-        case 4 -> "30";
-        case 5 -> "31";
-        case 6 -> "30";
-        case 7 -> "31";
-        case 8 -> "31";
-        case 9 -> "30";
-        case 10 -> "31";
-        case 11 -> "30";
-        case 12 -> "31";
         default -> "Ese mes no existe chaval";
     };
-
 }
 
-void validarNumero(int mes ) {
-
+void validarNumero(int mes) {
     if (mes >= 1 && mes <= 12) {
         IO.println(String.format("\nEl mes %d tiene %s días", mes, diasEnUnMes(mes)));
     } else if (mes == 0) {
@@ -31,11 +18,9 @@ void validarNumero(int mes ) {
 }
 
 void main() {
-    int mes = 0;
-
+    int mes;
     do {
         mes = Integer.parseInt(IO.readln("\nIntroduce el número de un mes (1-12) \"0 para salir\" : "));
         validarNumero(mes);
     } while (mes != 0);
-
 }
