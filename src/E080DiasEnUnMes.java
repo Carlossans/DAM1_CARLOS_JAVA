cc8deee017d3ef1226e33039e5f574708773a0dc
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-13 14:03:43 +0100

    E080

---
+++ src/E080DiasEnUnMes.java
@@ -0,0 +1,36 @@
+int diasEnUnMes(int mes) {
+
+    return switch (mes) {
+        case 1 -> 31;
+        case 2 -> 28;
+        case 3 -> 31;
+        case 4 -> 30;
+        case 5 -> 31;
+        case 6 -> 30;
+        case 7 -> 31;
+        case 8 -> 31;
+        case 9 -> 30;
+        case 10 -> 31;
+        case 11 -> 30;
+        case 12 -> 31;
+        default -> 0;
+    };
+}
+
+void validarNumero(int mes ) {
+
+    if (mes >= 1 || mes >= 12) {
+        diasEnUnMes(mes);
+    } else {
+        IO.println("\n FORMATO NO VÁLIDO DEBES DE INTRODUCIR UN NÚMERO");
+    }
+}
+
+void main() {
+    
+    int mes = Integer.parseInt(IO.readln("\nIntroduce el número de un mes (0-12): "));
+    validarNumero(mes);
+
+    IO.println(String.format("\nEl mes %d tiene %d días", mes, diasEnUnMes(mes)));
+
+}
