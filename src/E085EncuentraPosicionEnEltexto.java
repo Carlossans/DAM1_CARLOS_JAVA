0f8d6a5832b321702ed614c2ee487624eb53d5ac
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-17 13:42:38 +0100

    Problema con index out of bounds 11

--- src/E085EncuentraPosicionEnEltexto.java
+++ src/E085EncuentraPosicionEnEltexto.java
@@ -1,39 +1,34 @@
 int encuentraPosicionEnElTexto(String textoBuscado, String textoPrincipal) {
-    if (textoBuscado.isEmpty()) {
-        return 0;
-    }
-    
-    int longitudBuscada = textoBuscado.length();
+
     int longitudPrincipal = textoPrincipal.length();
-    
-    for (int i = 0; i <= longitudPrincipal - longitudBuscada; i++) {
+    int longitudBuscado = textoBuscado.length();
+
+    for (int i = 0; i <= longitudPrincipal - longitudBuscado; i++) {
         boolean encontrado = true;
-        
-        for (int j = 0; j < longitudBuscada; j++) {
+
+        for (int j = 0; j < longitudPrincipal; j++) {
             if (textoPrincipal.charAt(i + j) != textoBuscado.charAt(j)) {
                 encontrado = false;
                 break;
             }
         }
-        
         if (encontrado) {
             return i;
         }
     }
-    
     return -1;
 }
 
 void main() {
+
     String textoPrincipal = IO.readln("\nIntroduce el texto principal: ");
-    String textoBuscado = IO.readln("\nIntroduce el texto que quieres buscar en el texto principal: ");
-    
-    int posicion = encuentraPosicionEnElTexto(textoBuscado, textoPrincipal);
-    
-    if (posicion != -1) {
-        System.out.println("El texto '" + textoBuscado + "' se encontró en la posición: " + posicion);
-        IO.println(String.format("El texto %s se encontró en la posición", textoBuscado, posicion));
+    String textoBuscado = IO.readln("\nIntroduce el texto/frase que quieras buscan en el texto principal: ");
+
+    int pos = encuentraPosicionEnElTexto(textoBuscado, textoPrincipal);
+
+    if (encuentraPosicionEnElTexto(textoBuscado, textoPrincipal) != -1) {
+        IO.println(String.format("\nEl texto %s se ha encontrado en la posición %d", textoBuscado, pos));
     } else {
-        IO.println(String.format("El texto %s no se ha encontrado en el texto", textoBuscado, posicion));
+        IO.println(String.format("\nEl texto %s no se ha encontrado", textoBuscado));
     }
 }
