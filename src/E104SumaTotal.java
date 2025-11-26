ee176b024c59f3068f3a9999a23f29ea9c583b29
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-26 17:04:35 +0100

    Update input prompt and adjust array size to store sum result

--- src/E104SumaTotal.java
+++ src/E104SumaTotal.java
@@ -1,8 +1,8 @@
 void main() {
-    int nCasillas = Integer.parseInt(IO.readln("\nIntroduce cuantos números quieres introducir: ")), sumaTotal = 0;
-    int[] numerosALeer = new int [nCasillas];
+    int nCasillas = Integer.parseInt(IO.readln("\nIntroduce cuantos números quieres sumar: ")), sumaTotal = 0;
+    int[] numerosALeer = new int [nCasillas + 1];
 
-    for (int i = 0; i < numerosALeer.length; i++) {
+    for (int i = 0; i < numerosALeer.length - 1; i++) {
         IO.print(String.format("\nActualmente te encuentras en la casilla %d.", i + 1));
         numerosALeer[i] = Integer.parseInt(IO.readln(" Introduce un número que tu quieras: "));
     }
@@ -10,5 +10,7 @@ void main() {
     for (int numActual : numerosALeer) {
         sumaTotal = sumaTotal + numActual;
     }
-    IO.println(String.format("\nLa suma total de los números introducidos es: %d", sumaTotal));
+    numerosALeer[numerosALeer.length - 1] = sumaTotal;
+
+    IO.println(String.format("\nEl programa creó una casilla extra en la posición %d para guardar el resultado de la suma, que es: %d",numerosALeer.length, sumaTotal));
 }
