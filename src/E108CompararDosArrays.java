97e3890e807912207c4dc8d96c4b6c3ec5239ae0
Carlos <santosjimenezcarlos4@gmail.com>
2025-11-28 12:20:27 +0100

    Ejercicios arrays actualización

--- src/E108CompararDosArrays.java
+++ src/E108CompararDosArrays.java
@@ -1,65 +1,97 @@
 void main() {
-    int n1 = Integer.parseInt(IO.readln("\n¿Cuántos números quieres introducir en la lista1?\n")), n2 = java.lang.Integer.parseInt(IO.readln("\n¿Cuántos número quieres introducir en la lista2?\n")), diferencia, nose;
+    int n1 = Integer.parseInt(IO.readln("\n¿Cuántos números quieres introducir en la lista 1?\n")), n2 = java.lang.Integer.parseInt(IO.readln("\n¿Cuántos números quieres introducir en la lista 2?\n")), diferencia, nose;
 //
 //    verdaderoFalso1 = java.lang.Integer.parseInt(IO.readln("\n¿Cuántos números quieres introducir en la lista 1 de valores(Verdadero/Falso)?\n")), verdaderoFalso2 = Integer.parseInt(IO.readln("\n¿Cuántos números quieres introducir en la lista de decimales 2?\n"));
 
-    diferencia = (n1 > n2) ? n1 - n2: n2 - n1;
+    diferencia = (n1 > n2) ? n1 - n2 : (n2 > n1) ? n2 - n1 : 0;
 
     int[] lista1 = new int [n1];
     int[] lista2 = new int [n2];
 
-    if (lista1.length > lista2.length) {
+    if (diferencia == 0) {
+
+        IO.print("\n----COMIENZA LA LISTA 1 DE NÚMEROS ENTEROS----\n");
+        for (int i = 0; i < lista1.length; i++) {
+            IO.print(String.format("Actualmente te encuentras en la casilla %d de la lista de números enteros 1.", i + 1));
+            lista1[i] = Integer.parseInt(IO.readln(" Introduce el número que quieras: "));
+        }
+
+        IO.print("\n----COMIENZA LA LISTA 2 DE NÚMEROS ENTEROS----\n");
+        for (int i = 0; i < lista2.length; i++) {
+            IO.print(String.format("Actualmente te encuentras en la casilla %d de la lista de números enteros 2.", i + 1));
+            lista2[i] = Integer.parseInt(IO.readln(" Introduce el número que quieras: "));
+        }
+
+        IO.print("\n----COMIENZA LA COMPARACIÓN ENTRE LOS VALORES DE CADA CASILLA DE LOS ARRAYS----");
+        for (int i = 0; i < lista1.length; i++) {
+            IO.print(String.format(
+                    (lista1[i] == lista2[i]) ? "\nLos valores de ambas casillas en la posición %d son IGUALES" : (lista1[i] > lista2[i]) ? "\nEn la posición %d el valor %d de la lista 1 es MAYOR que el valor %d de la lista 2" : "\nEn la posición %d el valor %d de la lista 1 es MENOR que el valor %d de la lista 2"
+            , i + 1, lista1[i], lista2[i]));
+        }
+    } else if (lista1.length > lista2.length) {
         int[] lista21 = new int [n2 + diferencia];
 
         IO.print("\n----COMIENZA LA LISTA 1 DE NÚMEROS ENTEROS----\n");
         for (int i = 0; i < lista1.length; i++) {
-            IO.print(String.format("Acutalmente te encuentras en la casilla %d de la lista de números enteros 1.", i + 1));
+            IO.print(String.format("Actualmente te encuentras en la casilla %d de la lista de números enteros 1.", i + 1));
             lista1[i] = Integer.parseInt(IO.readln(" Introduce el números que quieras: "));
         }
 
         IO.print("\n----COMIENZA LA LISTA 2 DE NÚMEROS ENTEROS----\n");
         for (int i = 0; i < lista21.length - diferencia; i++) {
             IO.print(String.format("Acutalmente te encuentras en la casilla %d de la lista de números enteros 2.", i + 1));
-            lista2[i] = Integer.parseInt(IO.readln(" Introduce el números que quieras: "));
+            lista21[i] = Integer.parseInt(IO.readln(" Introduce el número que quieras: "));
         }
 
         nose = 0;
+        IO.print("\n----COMIENZA LA COMPARACIÓN ENTRE LOS VALORES DE CADA CASILLA DE LOS ARRAYS----");
         for (int i = 0; i < lista1.length; i++) {
             try {
                 IO.print(String.format(
-                        (lista1[i] == lista2[i]) ? "\nLos valores de ambas listas en al posición %d son iguales" : (lista1[i] > lista2[i]) ? "\nEl valor de la lista 1 en la posición %d es mayor" : "\nEl valor de la lista 2 en la posición %d es mayor"
-                        , i + 1));
+                        (lista1[i] == lista21[i]) ? "\nLos valores de ambas casillas en la posición %d son IGUALES" : (lista1[i] > lista21[i]) ? "\nEl valor %d de la lista 1 es MAYOR que el valor %d de la lista 2" : "\nEl valor %d de la lista 1 es MENOR que el valor %d de la lista 2"
+                        , i + 1, lista1[i], lista21[i]));
             } catch (ArrayIndexOutOfBoundsException e) {
                 IO.print("\nNO HAY VALOR EN ALGUNA DE LAS CASILLAS DE LA LISTA");
             }
         }
 
-    } else if (lista2.length > lista1.length) {
+    } else {
         int[] lista11 = new int [n1 + diferencia];
 
         IO.print("\n----COMIENZA LA LISTA 1 DE NÚMEROS ENTEROS----\n");
         for (int i = 0; i < lista11.length - diferencia; i++) {
             IO.print(String.format("Actualmente te encuentras en la casilla %d de la lista de números enteros 1.", i + 1));
-            lista1[i] = Integer.parseInt(IO.readln(" Introduce el números que quieras: "));
+            lista1[i] = Integer.parseInt(IO.readln(" Introduce el número que quieras: "));
         }
 
         IO.print("\n----COMIENZA LA LISTA 2 DE NÚMEROS ENTEROS----\n");
         for (int i = 0; i < lista2.length; i++) {
             IO.print(String.format("Actualmente te encuentras en la casilla %d de la lista de números enteros 2.", i + 1));
-            lista2[i] = Integer.parseInt(IO.readln(" Introduce el números que quieras: "));
+            lista2[i] = Integer.parseInt(IO.readln(" Introduce el número que quieras: "));
         }
 
         nose = 0;
+        IO.print("\n----COMIENZA LA COMPARACIÓN ENTRE LOS VALORES DE CADA CASILLA DE LOS ARRAYS----");
         for (int i = 0; i < lista2.length; i++) {
             try {
                 IO.print(String.format(
-                        (lista1[i] == lista2[i]) ? "\nLos valores de ambas listas en al posición %d son iguales" : (lista1[i] > lista2[i]) ? "\nEl valor de la lista 1 en la posición %d es mayor" : "\nEl valor de la lista 2 en la posición %d es mayor"
-                        , i + 1));
+                        (lista1[i] == lista2[i]) ? "\nLos valores de ambas casillas en la posición %d son IGUALES" : (lista1[i] > lista2[i]) ? "\nEl valor %d de la lista 1 es MAYOR que el valor %d de la lista 2" : "\nEl valor %d de la lista 1 es MENOR que el valor %d de la lista 2"
+                        , i + 1, lista1[i], lista2[i]));
             } catch (ArrayIndexOutOfBoundsException e) {
                 IO.print("\nNO HAY VALOR EN ALGUNA DE LAS CASILLAS DE LA LISTA");
             }
         }
     }
+
+//    diferencia = (decimales1 > decimales2) ? decimales1 - decimales2 : decimales2 - decimales1;
+//
+//    double[] verdaderoOFalso = new double [decimales1];
+//    double[] falsoOVerdadero = new double [decimales2];
+
+
+//    for ()  {
+//
+//    }
 
 
 //    double[] listaDecimales1 = new double [decimales1];
