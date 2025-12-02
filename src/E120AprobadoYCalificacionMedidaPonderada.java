b60ef2fb3a61781c6dddeb806f91f6fcbc96d218
Carlos <santosjimenezcarlos4@gmail.com>
2025-12-02 16:55:09 +0100

    me quedan cositas

--- src/E120AprobadoYCalificacionMedidaPonderada.java
+++ src/E120AprobadoYCalificacionMedidaPonderada.java
@@ -1,6 +1,40 @@
-void main() {
-    double[] calificaciones = {5.7, 9.5, 4.9, 0.0, 7.75};
-    double[] pesos = {0.15, 0.20, 0.25, 0.30, 0.10};
+boolean comprobarSiPuedeAprobar(double calificacion) {
+    return (calificacion < 4);
+}
 
-    //hacer un método con un bucle que compruebe si el número es >= 4, el método será boolean y me devolverá un true/false y el método recibirá un int
+//double calcularMedia(double calificacion, double peso) {
+//    double media = 0;
+//
+//    media = media + (calificacion * peso);
+//
+//    return media;
+//}
+
+
+void main() {
+    boolean aprueba;
+    int contador = 0;
+    double[] calificaciones = {9.0, 5.23, 4.0, 4.0, 4.0};
+    double[] pesos = {0.15, 0.20, 0.25, 0.30, 0.10};
+    double mediaTotal = 0;
+
+
+    for (double calificacion : calificaciones) {
+        if (comprobarSiPuedeAprobar(calificacion)) contador++;
+    }
+    aprueba = (contador < 1);
+
+    IO.println(
+            (aprueba) ? "\nEl alumno PUEDE aprobar DE MOMENTO... " : "\nEl alumno NO PUEDE aprobar"
+    );
+
+    if (aprueba) {
+        for (int i = 0; i < calificaciones.length; i++) {
+            mediaTotal = mediaTotal + (calificaciones[i] * pesos[i]);
+        }
+
+        IO.println(String.format(
+                (mediaTotal >= 5.00) ? "\nApruebas con un %.2f" : "\nAl final suspendes y se te queda la media en un %.2f"
+        , mediaTotal));
+    }
 }
