24416a1e76bc16960a87fce92047f750df932c5b
Carlos <santosjimenezcarlos4@gmail.com>
2025-12-02 12:57:45 +0100

    comment

--- src/E120AprobadoYCalificacionMedidaPonderada.java
+++ src/E120AprobadoYCalificacionMedidaPonderada.java
@@ -1,3 +1,6 @@
 void main() {
-    
+    double[] calificaciones = {5.7, 9.5, 4.9, 0.0, 7.75};
+    double[] pesos = {0.15, 0.20, 0.25, 0.30, 0.10};
+
+    //hacer un método con un bucle que compruebe si el número es >= 4
 }
