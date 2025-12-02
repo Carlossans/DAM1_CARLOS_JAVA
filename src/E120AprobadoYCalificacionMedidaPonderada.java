2dbc51d26cdf3b4107c97f20eef54c6722f77164
Carlos <santosjimenezcarlos4@gmail.com>
2025-12-02 13:03:55 +0100

    Act comment

--- src/E120AprobadoYCalificacionMedidaPonderada.java
+++ src/E120AprobadoYCalificacionMedidaPonderada.java
@@ -2,5 +2,5 @@ void main() {
     double[] calificaciones = {5.7, 9.5, 4.9, 0.0, 7.75};
     double[] pesos = {0.15, 0.20, 0.25, 0.30, 0.10};
 
-    //hacer un método con un bucle que compruebe si el número es >= 4
+    //hacer un método con un bucle que compruebe si el número es >= 4, el método será boolean y me devolverá un true/false y el método recibirá un int
 }
