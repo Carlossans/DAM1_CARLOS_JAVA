bea9075891e3309624a8ccb1d27f4161ec0ec929
Carlos <santosjimenezcarlos4@gmail.com>
2025-12-03 10:39:01 +0100

    cambio tonto porque soy retrasado

--- src/E116PromedioYmayorMenorConPosiciones.java
+++ src/E116PromedioYmayorMenorConPosiciones.java
@@ -2,15 +2,15 @@ void main() {
     int n, min, max, posicionMenor = 1, posicionMayor = 1;
     double sumaTotal = 0;
 
-    do {
-        n = Integer.parseInt(IO.readln("\nPor favor, introduzca la cantidad de números deseada: "));
-        if (n < 2) IO.println("\nEl número debe ser como mínimo 2, intentalo de nuevo.");
-    } while (n < 2);
+
+    n = Integer.parseInt(IO.readln("\nPor favor, introduzca la cantidad de números deseada: "));
 
     int[] numEnteros = new int [n];
 
     for (int i = 0; i < numEnteros.length; i++) {
-        numEnteros[i] = Integer.parseInt(IO.readln("\nIntroduzca el número " + (i + 1) + ": "));
+        do {
+            numEnteros[i] = Integer.parseInt(IO.readln("\nIntroduzca el número " + (i + 1) + ": "));
+        } while (numEnteros[i] < 2);
     }
 
     for (int numActual : numEnteros) {
