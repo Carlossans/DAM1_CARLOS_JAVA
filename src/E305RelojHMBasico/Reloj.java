1e93f68ea376376d5b0fc10fa0df9df6098e55ef
Carlos <santosjimenezcarlos4@gmail.com>
2026-01-15 14:08:24 +0100

    polla

---
+++ src/E305RelojHMBasico/Reloj.java
@@ -0,0 +1,64 @@
+package E305RelojHMBasico;
+
+public class Reloj {
+
+    private int h;
+    private int m;
+
+    public Reloj() {
+        this.h = 8;
+        this.m = 15;
+    }
+
+    public Reloj(int h, int m) {
+        int total = h * 60 + m;
+        formatearHora(total);
+
+    }
+
+    public Reloj(int totalMinutos) {
+        this.h = totalMinutos / 60;
+        this.m = totalMinutos % 60;
+    }
+
+    public void formatearHora(int totalMinutos) {
+        int minutosDia = 24 * 60;
+        totalMinutos %= minutosDia;
+        if (totalMinutos < 0) {
+            totalMinutos += minutosDia;
+        }
+        this.h = totalMinutos / 60;
+        this.m = totalMinutos % 60;
+    }
+
+
+    public void tick() {
+        sumarMinutos(1);
+    }
+
+    public void sumarMinutos(int minutos) {
+        int total = h * 60 + m + minutos;
+        formatearHora(total);
+    }
+
+    public void restarMinutos(int minutos) {
+        int total = h * 60 + m - minutos;
+        formatearHora(total);
+    }
+
+    public int diferenciaMinutos(Reloj otro) {
+        int t1 = this.h * 60 + this.m;
+        int t2 = otro.h * 60 + otro.m;
+        return t1 - t2;
+    }
+
+    public Reloj diferenciaReloj(Reloj otro) {
+        int diferencia = Math.abs(diferenciaMinutos(otro));
+        return new Reloj(diferencia);
+    }
+
+    public String toString() {
+        return String.format("%02d:%02d", h, m);
+    }
+}
+
