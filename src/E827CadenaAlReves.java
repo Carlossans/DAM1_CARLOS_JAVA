cc59cc9f7c8d6d6c6c2aa790e9a25b6eaabb2751
Carlos <santosjimenezcarlos4@gmail.com>
2026-01-26 10:12:14 +0100

    cadena al reves recursiva

---
+++ src/E827CadenaAlReves.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class E827CadenaAlReves {
+    public static void imprimirCadenaAlReves(String palabra) {
+
+        if (palabra.length() == 0) return;
+
+        System.out.print(palabra.charAt(palabra.length() - 1));
+
+        imprimirCadenaAlReves(palabra.substring(0, palabra.length() - 1));
+    }
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        String palabra = teclado.nextLine();
+
+        imprimirCadenaAlReves(palabra);
+
+    }
+}
