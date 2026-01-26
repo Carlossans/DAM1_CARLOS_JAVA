881a9650f359791ae0bba9047a076154dea441a1
Carlos <santosjimenezcarlos4@gmail.com>
2026-01-26 09:54:08 +0100

    potencia recursiva

---
+++ src/E825Potencia.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class E825Potencia {
+    public static int calcularPotencia(int num, int potencia) {
+        if (potencia == 0) return 1;
+        else return num * calcularPotencia(num, potencia - 1);
+    }
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int num = teclado.nextInt();
+        int potencia = teclado.nextInt();
+
+        System.out.println(calcularPotencia(num, potencia));
+    }
+}
