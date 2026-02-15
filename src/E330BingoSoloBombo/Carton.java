56c2a0b5203041dd81d8cf6b902e6db91877c63b
carlos <santosjimenezcarlos4@gmail.com>
2026-02-15 23:47:27 +0100

    bingo final

--- src/E330BingoSoloBombo/Carton.java
+++ src/E330BingoSoloBombo/Carton.java
@@ -4,73 +4,124 @@ import java.util.Random;
 import java.util.ArrayList;
 
 public class Carton {
-    private ArrayList<Integer> numeros;
-    private ArrayList<ArrayList<Integer>> filasNumeros = new ArrayList<>();
-    private ArrayList<ArrayList<Boolean>> filasMarcas = new ArrayList<>();
-    private ArrayList<Boolean> marcas;
+    private ArrayList<ArrayList<Integer>> filasNumeros;
+    private ArrayList<ArrayList<Boolean>> filasMarcas;
+
     private Bombo bombo;
     private Random aleatorio = new Random();
     private int id;
     private static int contadorId = 1;
+    private boolean cantoLinea = false;
+
     private static final String VERDE = "\u001B[32m";
     private static final String RESET = "\u001B[0m";
-    private static final int NUMFILAS = 0;
+    private static final int NUM_FILAS = 3;
 
     public Carton (Bombo bombo) {
-        int cantidadNumerosCalculada = Math.max(1, bombo.getTotalBolas() / 6);
-
         this.bombo = bombo;
-        this.id = contadorId++;
-        for (int i = 0; i < NUMFILAS; i++) {
+        id = contadorId++;
+        filasMarcas = new ArrayList<>();
+        filasNumeros = new ArrayList<>();
+
+        for (int i = 0; i < NUM_FILAS; i++) {
             filasNumeros.add(new ArrayList<Integer>());
             filasMarcas.add(new ArrayList<Boolean>());
         }
 
-        rellenarAleatoriamente(cantidadNumerosCalculada);
+        int totalNumeros = Math.max(NUM_FILAS, bombo.getTotalBolas() / 6);
+
+        rellenarAleatoriamente(totalNumeros);
     }
 
-    public void rellenarAleatoriamente(int cantidadNumeros) {
+    public void rellenarAleatoriamente(int totalNumeros) {
+        int numerosPorFila = totalNumeros / NUM_FILAS;
         int totalBolas = bombo.getTotalBolas();
 
-        while (numeros.size() < cantidadNumeros) {
-            int num = aleatorio.nextInt(totalBolas) + 1;
+        for (int i = 0; i < NUM_FILAS; i++) {
+            ArrayList<Integer> filaActual = filasNumeros.get(i);
+            ArrayList<Boolean> filaMarcas = filasMarcas.get(i);
 
-            if (!numeros.contains(num)) {
-                numeros.add(num);
-                marcas.add(false);
+            while (filaActual.size() < numerosPorFila) {
+                int numeroAleatorio = aleatorio.nextInt(totalBolas) + 1;
+
+                if (!elCartonYaTieneNum(numeroAleatorio)) {
+                    filaActual.add(numeroAleatorio);
+                    filaMarcas.add(false);
+                }
+                
             }
         }
     }
 
-    public void anotaBola (int bola) {
-        for (int i = 0; i < numeros.size(); i++) {
-            if (numeros.get(i) == bola) {
-                marcas.set(i, true);
+    public boolean elCartonYaTieneNum(int num) {
+        for (ArrayList<Integer> fila : filasNumeros) {
+            if (fila.contains(num)) return true;
+        }
+        return false;
+    }
+
+    public void anotaBola(int bola) {
+        for (int i = 0; i < NUM_FILAS; i++) {
+            for (int j = 0; j < filasNumeros.get(i).size(); j++) {
+                if (filasNumeros.get(i).get(j) == bola) {
+                    filasMarcas.get(i).set(j, true);
+                }
             }
         }
+    }
+
+    public boolean hayLinea() {
+        if (cantoLinea) return false;
+
+        for (ArrayList<Boolean> filaMarcas : filasMarcas) {
+            boolean lineaCompleta = true;
+            for (Boolean marcada : filaMarcas) {
+                if (!marcada) {
+                    lineaCompleta = false;
+                    break;
+                }
+            }
+            if (lineaCompleta) {
+                cantoLinea = true;
+                return true;
+            }
+        }
+        return false;
     }
 
     public boolean hayBingo() {
-        for (Boolean marcada : marcas) {
-            if(!marcada) {
-                return false;
+        for (ArrayList<Boolean> filaMarcas : filasMarcas) {
+            for (Boolean marcada : filaMarcas) {
+                if(!marcada) {
+                    return false;
+                }
             }
         }
         return true;
     }
 
+    public int getId() {
+        return this.id;
+    }
+
     public String toString() {
-        StringBuilder carton = new StringBuilder();
-        carton.append("CARTÓN ").append(id).append(": ");
-
-        for (int i = 0; i < numeros.size(); i++) {
-            if (marcas.get(i)) {
-                carton.append("[").append(VERDE).append(numeros.get(i)).append(RESET).append("] ");
-            } else {
-                carton.append(numeros.get(i)).append(" ");
+        StringBuilder sb = new StringBuilder();
+        sb.append("CARTÓN ").append(id).append(":\n");
+        
+        for (int f = 0; f < NUM_FILAS; f++) {
+            sb.append("  "); // Margen
+            for (int c = 0; c < filasNumeros.get(f).size(); c++) {
+                int num = filasNumeros.get(f).get(c);
+                boolean marcada = filasMarcas.get(f).get(c);
+                
+                if (marcada) {
+                    sb.append("[").append(VERDE).append(String.format("%02d", num)).append(RESET).append("] ");
+                } else {
+                    sb.append(String.format("%02d", num)).append("   ");
+                }
             }
+            sb.append("\n");
         }
-
-        return carton.toString();
+        return sb.toString();
     }
 }
--- src/E330BingoSoloBombo/Principal.java
+++ src/E330BingoSoloBombo/Principal.java
@@ -6,38 +6,52 @@ import static java.lang.Thread.sleep;
 
 public class Principal {
     private static final String AZUL = "\u001B[34m";
+    private static final String AMARILLO = "\u001B[33m";
     private static final String RESET = "\u001B[0m";
 
     public static void main(String[] args) throws InterruptedException {
         Random aleatorio = new Random();
-        Bombo bombo = new Bombo(aleatorio.nextInt(15, 91));
+        Bombo bombo = new Bombo(aleatorio.nextInt(60, 91));
         ArrayList<Carton> cartones = new ArrayList<>();
         ArrayList<Carton> ganadores = new ArrayList<>();
-        boolean alguienTieneBingo = false;
 
-        int numCartones = aleatorio.nextInt(1, 6);
+        boolean alguienTieneBingo = false;
+        boolean lineaYaCantadaEnPartida = false;
+
+        int numCartones = aleatorio.nextInt(2, 6);
+
         for (int i = 0; i < numCartones; i++) {
             cartones.add(new Carton(bombo));
         }
 
-        System.out.printf("\n%sBIENVENIDO AL GRAN BINGO DEL I.E.S LAGUNA DE JOATZEL%s\n", AZUL, RESET);
+        System.out.println("\n".repeat(30));
+        System.out.printf("%sBIENVENIDO AL GRAN BINGO DEL I.E.S LAGUNA DE JOATZEL%s\n", AZUL, RESET);
 
         System.out.printf(
-                (numCartones  > 1 ) ? "\nEl BOMBO TIENE %d BOLAS Y EL JUEGO TIENE %d CARTONES\n\n" : "\nEl BOMBO TIENE %d BOLAS Y EL JUEGO TIENE %d CARTÓN\n\n"
-        ,bombo.getTotalBolas(), numCartones);
+                (numCartones > 1) ? "\nEL BOMBO TIENE %d BOLAS Y EL JUEGO CUENTA CON %d CARTONES\n\n"
+                        : "\nEL BOMBO TIENE %d BOLAS Y EL JUEGO CUENTA CON %d CARTÓN\n\n",
+                bombo.getTotalBolas(), numCartones);
 
         for (Carton carton : cartones) {
-            System.out.println(carton + "\n");
+            System.out.println(carton);
         }
+
+        sleep(3000);
 
         while (bombo.hayBolas() && !alguienTieneBingo) {
             int bola = bombo.dameBola();
 
-            System.out.println("\n".repeat(10));
-            System.out.printf("BOLA: %s %d %s\n\n", AZUL, bola, RESET);
+            System.out.println("\n".repeat(30));
+            System.out.printf("BOLA: %s%02d%s\n", AZUL, bola, RESET);
 
             for (Carton carton : cartones) {
                 carton.anotaBola(bola);
+
+                if (!lineaYaCantadaEnPartida && carton.hayLinea()) {
+                    System.out.printf("\n%sLÍNEA EN EL CARTÓN %d%s\n", AMARILLO, carton.getId(), RESET);
+                    lineaYaCantadaEnPartida = true;
+                    sleep(2000);
+                }
 
                 if (carton.hayBingo()) {
                     alguienTieneBingo = true;
@@ -46,32 +60,28 @@ public class Principal {
             }
 
             for (Carton carton : cartones) {
-                System.out.println(carton + "\n");
+                System.out.println(carton);
             }
-            //sleep(4000);
 
-            if (alguienTieneBingo) {
-                System.out.println("\n".repeat(10));
-                System.out.println("ÚLTIMA BOLA SACADA: " + bola);
+            if (!alguienTieneBingo) {
+                sleep(2500);
+            }
+        }
 
-                if (ganadores.size() > 1) {
-                    System.out.println("\nESTOS CARTONES GANADORES HAN CANTADO BINGO AL MISMO TIEMPO\n");
-
-                    for (Carton ganador : ganadores) {
-                        System.out.println(ganador + "\n");
-                    }
-                } else {
-                    System.out.println("\nEL CARTÓN QUE HA CANTADO BINGO ES El:\n");
-                    System.out.println(ganadores.getFirst() + "\n");
+        System.out.println("\n".repeat(5));
+        if (alguienTieneBingo) {
+            if (ganadores.size() > 1) {
+                System.out.println("HAY EMPATE, LOS SIGUIENTES CARTONES HAN CANTADO BINGO:");
+                for (Carton ganador : ganadores) {
+                    System.out.println("\n" + ganador);
                 }
+            } else {
+                System.out.printf("\n%sBINGO, EL CARTÓN GANADOR ES:%s\n %s\n", AMARILLO, RESET, ganadores.getFirst());
             }
+        } else {
+            System.out.println("Se han acabado las bolas. Nadie cantó bingo");
         }
 
-        if (!alguienTieneBingo) {
-            System.out.println("Se han sacado todas las bolas del bombo y no hay ganadores");
-        }
-
-        System.out.println("\n".repeat(10));
         System.out.println(bombo);
     }
 }
